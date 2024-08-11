e1b2abf9998d698898344968a7ed23a44d21b565
Animesh Mukherjee <37202195+Animesh66@users.noreply.github.com>
2024-08-11 19:17:58 +0200

    Update linked_list.py

--- linked_list.py
+++ linked_list.py
@@ -26,25 +26,52 @@ class LinkedList(_Node):
         # if the node is first item
         if self.head is None:
             self.head = self.tail = new_node
-            self.count = 1
         # if there are already items in the link list
-        self.tail.next = new_node
-        self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
         self.count += 1
+        return True
 
     def pop(self):
         """
         Method for removing element at the end of link list
         """
         # case 1: if there are no items in the link list
+        if self.head is None:
+            return None
+        # case 2: if there are more than one item in the link list
+        current = previous = self.head
+        while current.next:
+            previous = current
+            current = current.next
+        self.tail = previous
+        self.tail.next = None
+        self.count -= 1
+        # case 3: if there are only one item in the link list
+        if self.count == 0:
+            self.head = self.tail = None
+        return current
 
-        # case 2: if there are only one item in the link list
-
-        # case 3: if there are more than one item in the link list
+    def prepend(self, item):
+        """
+        Method for adding an element at the begining of the list
+        Args:
+            item (int): the value to be put node
+        """
+        new_node = _Node(item)
+        # case 1: if there are no element in the list
+        if self.head is None:
+            self.head = self.tail = new_node
+        # case 2: if there are one or more element in the list
+        else:
+            new_node.next = self.head
+            self.head = new_node
+        self.count += 1
 
 
 linked_list = LinkedList()
-linked_list.append(3)
-linked_list.append(4)
-linked_list.append(5)
+linked_list.prepend(4)
+linked_list.append(1)
 linked_list.print_list()
+print(linked_list.count)
