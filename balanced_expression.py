f5bfb9fa82bcac8c28899585958e454898dc3e78
Animesh Mukherjee <37202195+Animesh66@users.noreply.github.com>
2024-08-24 22:21:44 +0200

    Update balanced_expression.py

--- balanced_expression.py
+++ balanced_expression.py
@@ -4,12 +4,16 @@ from stack import Stack
 def balanced_expression(input_expression: str) -> bool:
     stack = Stack()
     for char in input_expression:
+        # check if its an opening bracket then push it in the stack
         if char == "(" or char == "<" or char == "[" or char == "{":
             stack.push(char)
+        # if the bracket is a closing brack then we need to pop the top item
         if char == ")" or char == ">" or char == "]" or char == "}":
+            #  if the char is a closing bracket and stack is empty then not blanced
             if stack.is_empty():
                 return False
             top = stack.pop()
+        # check if the current char is closing bracket and top is not opening bracket then not balanced
             if char == ")" and top != "(":
                 return False
             if char == ">" and top != "<":
@@ -18,6 +22,7 @@ def balanced_expression(input_expression: str) -> bool:
                 return False
             if char == "}" and top != "{":
                 return False
+    # retrun true if the stack is empty after this operation otherwise return false
     return stack.is_empty()
 
 
