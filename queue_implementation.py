7b9299f27272c24799eca3c7f2a494bd6f1389cb
Animesh Mukherjee <37202195+Animesh66@users.noreply.github.com>
2024-08-24 22:53:42 +0200

    Update queue_implementation.py

--- queue_implementation.py
+++ queue_implementation.py
@@ -1,59 +1,73 @@
+from stack import Stack
+
+
 class _Node:
 
     def __init__(self, value) -> None:
-        self.value: int | str = value
+        self.value = value
         self.next = None
 
 
-class Stack(_Node):
+class Queue(_Node):
 
     def __init__(self) -> None:
-        self.top = None
+        self.first = None
+        self.last = None
         self.length = 0
 
-    def print_stack(self):
-        current = self.top
+    def print_queue(self) -> None:
+        current = self.first
         while current:
             print(current.value)
             current = current.next
 
-    def push(self, value) -> bool:
+    def enque(self, value) -> bool:
         new_node = _Node(value=value)
-        # case 1: check if the stack is empty
-        if self.top is None:
-            self.top = new_node
-        # case 2: if there are at least one or more items in the stack
+        # case 1: verify if the queue is empty
+        if self.first is None:
+            self.first = self.last = new_node
+        # case 2: if there are more than one element in the queue
         else:
-            new_node.next = self.top
-            self.top = new_node
+            self.last.next = new_node
+            self.last = new_node
         self.length += 1
         return True
 
-    def pop(self) -> _Node | None:
-        # case 1: check if the stack is empty or not
-        if self.top is None:
+    def deque(self) -> _Node | None:
+        # case 1: check if the queue is empty or not
+        if self.first is None:
             return None
-        # if there are one or more item in the stack
+        current = self.first
+        # case 2: if there are only one item in queue
+        if self.length == 1:
+            self.first = self.last = None
+        # case 3: check if the queue is more than one element in queue
         else:
-            current = self.top
-            self.top = current.next
+            self.first = self.first.next
             current.next = None
         self.length -= 1
         return current.value
 
-    def peek(self) -> _Node | None:
-        # case 1: check if the stack is empty
-        if self.top is None:
-            return None
-        else:
-            return self.top
-
-    def is_empty(self) -> bool:
+    def is_empty(self):
         return self.length == 0
 
+    def peek(self):
+        return self.first
 
-stack = Stack()
-stack.push("a")
-stack.push("b")
-stack.push("c")
-print(stack.is_empty())
+    def reverse(self):
+        stack = Stack()
+        # while the queue is not empty then remove elements from queue and push it in stack
+        while not queue.is_empty():
+            stack.push(queue.deque())
+        # while the stack is not empty then remove elements from stack and put it inside the queue
+        while not stack.is_empty():
+            queue.enque(stack.pop())
+
+
+queue = Queue()
+queue.enque(10)
+queue.enque(20)
+queue.enque(30)
+queue.print_queue()
+queue.reverse()
+queue.print_queue()
