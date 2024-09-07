e7887d90db0b8aa73c1fb5d95aeb7a287681c203
Animesh Mukherjee <37202195+Animesh66@users.noreply.github.com>
2024-09-07 20:06:30 +0200

    Update avl_tree.py

--- avl_tree.py
+++ avl_tree.py
@@ -25,7 +25,7 @@ class AVLTree(_Node):
         # once the child is added we need to set the hight
         root._height = max(self.__height(root.left),
                            self.__height(root.right)) + 1
-        self.balanced(root)
+        root = self.balanced(root)
         return root
 
     def insert(self, value: int) -> int:
@@ -74,7 +74,7 @@ class AVLTree(_Node):
         """
         return self.balanced_factor(node) < -1
 
-    def balanced(self, node: _Node) -> None:
+    def balanced(self, node: _Node) -> _Node:
         """
         Returns the rotation required for an AVL tree.
         Args:
@@ -84,15 +84,49 @@ class AVLTree(_Node):
             # caes 1: if the tree is left heavy tree perform right rotation
             # on root and before left rotation on left child.
             if self.balanced_factor(node.left) < 0:
-                print(f"Left rotate {node.left.value}")
-            print(f"Right rotate {node.value}")
+                node.left = self.rotate_left(node.left)
+            return self.rotate_right(node)
         # case 2: if the tree is a right heavy tree.
         if self.is_right_heavy(node):
             # case 1: if the balanced factor of the right child of the node is negative
             # case 2: if the balaced factor of the right child of the node is positive
             if self.balanced_factor(node.right) > 0:
-                print(f"Right rotate {node.right.value}")
-            print(f"Left rotate {node.value}")
+                node.right = self.rotate_right(node.right)
+            return self.rotate_left(node)
+        return node
+
+    def set_height(self, node: _Node) -> None:
+        """
+        set the height of a given node.
+        """
+        node._height = max(self.__height(node.left),
+                           self.__height(node.right)) + 1
+
+    def rotate_left(self, node: _Node) -> _Node:
+        """
+        This method will rotate the given node to left
+        """
+        new_root = node.right
+        node.right = new_root.left
+        new_root.left = node
+
+        self.set_height(node)
+        self.set_height(new_root)
+
+        return new_root
+
+    def rotate_right(self, node: _Node) -> _Node:
+        """
+        This method will rotate the given node to right
+        """
+        new_root = node.left
+        node.left = new_root.right
+        new_root.right = node
+
+        self.set_height(node)
+        self.set_height(new_root)
+
+        return new_root
 
     def pre_order_traverse(self, root: _Node):
         # This is the solution for pre order traverse using recursion
