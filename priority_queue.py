295fb60a46bf1d48950c15e1bc795b86c097c287
Animesh Mukherjee <37202195+Animesh66@users.noreply.github.com>
2024-09-09 18:35:19 +0200

    Create priority_queue.py

---
+++ priority_queue.py
@@ -0,0 +1,32 @@
+from heap import MaxHeap
+
+
+class PriorityQueue:
+
+    def __init__(self) -> None:
+        self.heap = MaxHeap()
+
+    def enqueue(self, value: int) -> None:
+        """
+        method to insert an element in on a priority queue.
+        """
+        self.heap.insert(value)
+
+    def dequeue(self) -> int:
+        """
+        Method to remove the largest element from a queue.
+        """
+        return self.heap.remove()
+
+    def is_empty(self) -> bool:
+        """
+        Helper method to return true if the priority queue is empty.
+        """
+        return self.heap._is_empty()
+
+
+priority_queue = PriorityQueue()
+priority_queue.enqueue(10)
+priority_queue.enqueue(7)
+priority_queue.enqueue(22)
+priority_queue.heap
