389fc22ea8726ef828cfbd096892aa69fc3cccb4
Animesh Mukherjee <37202195+Animesh66@users.noreply.github.com>
2024-09-10 14:12:54 +0200

    Update heapify.py

--- heapify.py
+++ heapify.py
@@ -1,38 +1,50 @@
 class Heapfy:
 
-    def heapify(self, input_list: list[int]) -> list[int]:
+    @classmethod
+    def heapify(cls, input_list: list[int]) -> list[int]:
         """
         Method to heapify an provided list.
         """
-        for index, value in enumerate(input_list):
-            self.__heapify(input_list, index)
+        for index in range(len(input_list)):
+            cls.__heapify(input_list, index)
+        return input_list
 
-    def __heapify(self, input_list: list[int], index: int) -> list[int]:
+    @classmethod
+    def __heapify(cls, input_list: list[int], index: int) -> None:
         """
-        helper method to perfrom recursion for heapify.
+        helper method to perform recursion for heapify.
         """
         # Assume that the root index is larger index
         larger_index = index
-        #  calculate the values of left and right child index.
-        left_child_index = index * 2 + 1
-        right_child_index = index * 2 + 2
+        # Calculate the values of left and right child index.
         # verify if the left child index value is greater than the larger index value
         # and left child index is a valid index
-        if left_child_index < len(input_list) and input_list[left_child_index] > input_list[larger_index]:
+        left_child_index = index * 2 + 1
+        if (left_child_index < len(input_list)) \
+                and (input_list[left_child_index] > input_list[larger_index]):
             larger_index = left_child_index
         # verify if the right child index value is greater than the larger index value
         # and right child index is a valid index
-        if right_child_index < len(input_list) and input_list[right_child_index] > input_list[larger_index]:
+        right_child_index = index * 2 + 2
+        if (right_child_index < len(input_list)) and \
+                (input_list[right_child_index] > input_list[larger_index]):
             larger_index = right_child_index
-        # if the index is equals to the larger index that means its in correct place. hen do nothing
+        # if the item in the index is greater than both of the left and right child
+        # then the item is in correct position so return.
+        # This is the base condition of the recursion.
         if index == larger_index:
             return
         # Now swap the value of the list with larger item\
-        self.__swap(index, larger_index, input_list)
-        self.__heapify(input_list, larger_index)
+        cls.__swap(index, larger_index, input_list)
+        cls.__heapify(input_list, larger_index)
 
-    def __swap(self, first: int, second: int, input_list: int):
+    @classmethod
+    def __swap(cls, first: int, second: int, input_list: list[int]):
         """
         This method swaps the values of the items.
         """
         input_list[first], input_list[second] = input_list[second], input_list[first]
+
+
+convert_heap = [2, 5, 15, 34, 7]
+print(Heapfy.heapify(convert_heap))
