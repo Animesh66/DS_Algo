599f4df8a1be3ff15edbd2d3dee522999a122ee6
Animesh Mukherjee <37202195+Animesh66@users.noreply.github.com>
2024-09-17 18:45:35 +0200

    Update sorting.py

--- sorting.py
+++ sorting.py
@@ -3,11 +3,16 @@ def bubble_sort(input_list: list[int]):
     This method will sort the input list using bubble sort algorithm.
     time complexity for this algorithm is O(n^2)
     """
+    is_sorted = True  # This is an improvement in case the array is sorted.
     for i in range(len(input_list)):
-        #  in each pass if the value of the next element greater that next then swap the elements.
-        for j in range(1, len(input_list)):
+        #  In each pass if the value of the next element greater that next then swap the elements.
+        for j in range(1, len(input_list) - i):
             if input_list[j] < input_list[j - 1]:
                 input_list[j], input_list[j-1] = input_list[j-1], input_list[j]
+                is_sorted = False
+        # if the list is already sorted then we will not run the loop again.
+        if is_sorted:
+            return
 
     return input_list
 
