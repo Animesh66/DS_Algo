746d56f603c9d5b317ebe6eb23c2636d4fcfd889
Animesh Mukherjee <37202195+Animesh66@users.noreply.github.com>
2024-09-18 19:12:27 +0200

    Update sorting.py

--- sorting.py
+++ sorting.py
@@ -1,7 +1,7 @@
-def bubble_sort(input_list: list[int]):
+def bubble_sort(input_list: list[int]) -> list[str]:
     """
     This method will sort the input list using bubble sort algorithm.
-    time complexity for this algorithm is O(n^2)
+    time complexity for this algorithm is O(n^2) in worst case(sorted in reverse) and O(n) in bests case(list already sorted)
     """
     is_sorted = True  # This is an improvement in case the array is sorted.
     for i in range(len(input_list)):
@@ -17,19 +17,41 @@ def bubble_sort(input_list: list[int]):
     return input_list
 
 
-def selection_sort(input_list: list[int]):
+def selection_sort(input_list: list[int]) -> list[str]:
     """
     This function will sort the list using selection sort algorithm.
     We assume the first index is the minimum value and compare the elements with 
     other and swap the elements if needed.
-    The time complexity for this algorithm is O(n^2)
+    The time complexity for this algorithm is O(n^2) in both best case(list already sorted) and worst case(sorted in reverse).
     """
     for i in range(len(input_list)):
         min_index = i
         for j in range(i, len(input_list)):
             if input_list[j] < input_list[min_index]:
                 min_index = j
-        input_list[i], input_list[min_index] = input_list[min_index], input_list[i]
+        if i != min_index:  # Swap only when needed otherwise not required.
+            input_list[i], input_list[min_index] = input_list[min_index], input_list[i]
+    return input_list
+
+
+def insertion_sort(input_list: list[str]) -> list[str]:
+    """
+    This function will sort the list using insertion sort algorithm.
+    We loop though all the element and sort them in place instead of swaping the elements we shift elemets to wright.
+    other and swap the elements if needed.
+    The time complexity for this algorithm is O(n) in best case(list already sorted) and O(n^2) in worst case(srted in reverse).
+    """
+    # We are starting with second element in the list assuming first element is in corect place.
+    # Once we go to the 2nd element we will verify if the element is greater or not.
+    for i in range(1, len(input_list)):
+        # set the current element to the ith element to compare it with previous element.
+        current = input_list[i]
+        # in the inner loop we will compare the value of the previous element with current element.
+        j = i - 1
+        while (j >= 0 and input_list[j] > current):
+            input_list[j + 1] = input_list[j]
+            j -= 1
+        input_list[j + 1] = current
     return input_list
 
 
