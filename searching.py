a83621b4ad7c966e3c7dacb38644a01b1f9d268a
Animesh Mukherjee <37202195+Animesh66@users.noreply.github.com>
2024-09-20 19:39:40 +0200

    Update searching.py

--- searching.py
+++ searching.py
@@ -1,4 +1,14 @@
-def liner_search(input_list: list[int]):
+def liner_search(input_list: list[int], item: int) -> int:
     """
     This function illustrates the liner search algorithm.
+    This method takes an list and the rquired item to search and return the index of the search item.
+    in case the element does not exists then it returns -1
     """
+    for index, element in enumerate(input_list):
+        if item == element:
+            return index
+    return -1
+
+
+input_list = [13, 10, 8, 22, 59, 55]
+print(liner_search(input_list, 50))
