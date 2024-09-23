9e5853e613c37ab89666c1d70edd77f6fc7139bc
Animesh Mukherjee <37202195+Animesh66@users.noreply.github.com>
2024-09-23 15:33:28 +0200

    Update string_manipulation.py

--- string_manipulation.py
+++ string_manipulation.py
@@ -1,4 +1,9 @@
 def count_vowels(input_string: str) -> int:
+    """
+    This method will take a input string and returns the count of the vowels on that string.
+    """
+    if not input_string:
+        return 0
     vowels = "aeiou"
     count = 0
     for char in input_string:
@@ -6,7 +11,23 @@ def count_vowels(input_string: str) -> int:
             count += 1
     return count
 
-def string_reverse(input_str: str) -> str:
-    
 
-print(count_vowels("My name is Animeh Muherjee"))
+def string_reverse(input_str: str) -> str | None:
+    """
+    This method will take an input string as an input and output the reverse of that string. 
+    """
+    if input_str:
+        return input_str[::-1]
+    return None
+
+
+def word_reverse(input_sentence: str) -> str:
+    """
+    This method will take input an sentence and outputs the reverse of the sentence.
+    """
+    words = input_sentence.split(" ")
+    reverse_words = " ".join(words[::-1])
+    return reverse_words
+
+
+print(word_reverse("My name is Animesh"))
