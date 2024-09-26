97392fdce16c50346dd4fbb9b75a20bbdc9fada4
Animesh Mukherjee <37202195+Animesh66@users.noreply.github.com>
2024-09-26 18:43:38 +0200

    Update leetcode_solutions.py

--- leetcode_solutions.py
+++ leetcode_solutions.py
@@ -15,3 +15,22 @@ missing_number([1, 2, 3, 4, 6], 6) # 5
 def missing_number(arr, n):
     pass
 
+
+"""
+Find first non repeated character of a given string.
+"""
+
+
+def find_first_non_repeated_char(input_str: str) -> str:
+    char_dict = {}
+    for char in input_str:
+        if char not in char_dict:
+            char_dict[char.lower()] = 1
+        else:
+            char_dict[char.lower()] += 1
+    for char, frequency in char_dict.items():
+        if frequency == 1:
+            return char
+    return None
+
+print(find_first_non_repeated_char("I am a very good boi"))
