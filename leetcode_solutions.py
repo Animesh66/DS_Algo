d66f072b9b3d4bbe72f63c43f5814b1907673a09
Animesh Mukherjee <37202195+Animesh66@users.noreply.github.com>
2024-09-26 15:53:03 +0200

    Update leetcode_solutions.py

--- leetcode_solutions.py
+++ leetcode_solutions.py
@@ -1,1 +1,17 @@
+"""
+Missing Number
+Write a function to find the missing number 
+in a given integer array of 1 to 100. 
+The function takes two parameter 
+the array and the number of elements 
+that needs to be in array.  
+For example if we want to find missing number 
+from 1 to 6 the second parameter will be 6.
+Example
+missing_number([1, 2, 3, 4, 6], 6) # 5
+"""
+
+
+def missing_number(arr, n):
+    pass
 
