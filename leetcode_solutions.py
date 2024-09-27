c135e79bf0fb27ac502b35ed7cb67112065543bc
Animesh Mukherjee <37202195+Animesh66@users.noreply.github.com>
2024-09-27 19:05:56 +0200

    Update leetcode_solutions.py

--- leetcode_solutions.py
+++ leetcode_solutions.py
@@ -93,5 +93,18 @@ def find_duplicate(input_list: list[int]) -> int:
     return duplicate_value
 
 
+"""
+Given an integer array nums sorted in non-decreasing order, remove the duplicates 
+in-place such that each unique element appears only once. 
+The relative order of the elements should be kept the same. 
+Then return the number of unique elements in nums.
+Consider the number of unique elements of nums to be k, 
+to get accepted, you need to do the following things:
+Change the array nums such that the first k elements of nums contain the unique elements 
+in the order they were present in nums initially. 
+The remaining elements of nums are not important as well as the size of nums.
+Return k.
+"""
+
 input_list = [1, 3, 4, 2, 2]
 print(find_duplicate(input_list))
