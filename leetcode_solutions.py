115672c6b4b8828facfa9661b5c269a6c359bf71
Animesh Mukherjee <37202195+Animesh66@users.noreply.github.com>
2024-09-28 16:58:57 +0200

    Update leetcode_solutions.py

--- leetcode_solutions.py
+++ leetcode_solutions.py
@@ -104,7 +104,50 @@ Change the array nums such that the first k elements of nums contain the unique 
 in the order they were present in nums initially. 
 The remaining elements of nums are not important as well as the size of nums.
 Return k.
-"""
+Example 2:
 
-input_list = [1, 3, 4, 2, 2]
-print(find_duplicate(input_list))
+Input: nums = [0,0,1,1,1,2,2,3,3,4]
+Output: 5, nums = [0,1,2,3,4,_,_,_,_,_]
+Explanation: Your function should return k = 5, with the first five elements of nums being 0, 1, 2, 3, and 4 respectively.
+It does not matter what you leave beyond the returned k (hence they are underscores).
+"""
+def find_unique_elements_list(input_list: list[int]) -> list[int]:
+    uniqe_set = set()
+    for element in input_list:
+        if element not in input_list:
+            uniqe_set.add(element)
+        else:
+            input_list.remove(element)
+    return input_list
+
+"""
+Given the array of integers nums, you will choose two different indices i and j of that array. Return the maximum value of (nums[i]-1)*(nums[j]-1).
+Example 1:
+Input: nums = [3,4,5,2]
+Output: 12 
+Explanation: If you choose the indices i=1 and j=2 (indexed from 0), you will get the maximum value, that is, (nums[1]-1)*(nums[2]-1) = (4-1)*(5-1) = 3*4 = 12.
+"""
+def find_maximum_product(input_list: list[int]) -> int:
+    pass
+
+"""
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+You can return the answer in any order.
+Example 1:
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+"""
+def find_two_sum(input_list: list[int], target: int) -> list[int]:
+    seen = {}
+    for index, num in enumerate(input_list): 
+         complement = target - num
+         if complement in seen:
+            return [seen[complement], index]
+         else:
+            seen[num] = index
+
+
+input_list = [2,7,11,15]
+print(find_two_sum(input_list, 9))
