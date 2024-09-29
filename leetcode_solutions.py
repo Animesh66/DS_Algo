9b43938403ab2a6b00e4778156559553fa947d99
Animesh Mukherjee <37202195+Animesh66@users.noreply.github.com>
2024-09-29 21:06:40 +0200

    Update leetcode_solutions.py

--- leetcode_solutions.py
+++ leetcode_solutions.py
@@ -518,3 +518,40 @@ class Solution(object):
         prev.next = l1 if l1 is not None else l2
  
         return prehead.next
+
+"""
+Given the head of a sorted linked list, delete all duplicates such that each element appears only once. Return the linked list sorted as well. 
+Example 1:
+Input: head = [1,1,2]
+Output: [1,2]
+Example 2:
+Input: head = [1,1,2,3,3]
+Output: [1,2,3]
+"""
+
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+ 
+class Solution(object):
+    def deleteDuplicates(self, head):
+        if not head:
+            return None
+        
+        seen = set()
+        dummy = ListNode(-1)
+        dummy.next = head
+        prev_node = dummy
+        current_node = head
+ 
+        while current_node:
+            if current_node.val in seen:
+                prev_node.next = current_node.next
+                current_node = current_node.next
+            else:
+                seen.add(current_node.val)
+                prev_node = current_node
+                current_node = current_node.next
+        
+        return dummy.next
