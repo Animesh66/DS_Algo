e2b081ecbb935a4bdc4b4e151449d3e1762e4174
Animesh Mukherjee <37202195+Animesh66@users.noreply.github.com>
2024-09-29 20:39:42 +0200

    Update leetcode_solutions.py

--- leetcode_solutions.py
+++ leetcode_solutions.py
@@ -444,3 +444,15 @@ def concatenate_strings(input_tuple):
     return " ".join(input_tuple)
 
 # print(concatenate_strings(('Hello', 'World', 'from', 'Python')))
+
+"""
+Create a function that takes a tuple of tuples and returns a tuple containing the diagonal elements of the input.
+Example
+input_tuple = ((1, 2, 3),(4, 5, 6),(7, 8, 9))
+output_tuple = get_diagonal(input_tuple)
+print(output_tuple)  # Expected output: (1, 5, 9)
+"""
+def get_diagonal(input_tuple):
+    return tuple(input_tuple[i][i] for i in range(len(input_tuple)))
+
+print(get_diagonal(((1, 2, 3),(4, 5, 6),(7, 8, 9))))
