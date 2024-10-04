dc0e09aab0c7bae670f9fea1ad5631aca4153f57
Animesh Mukherjee <37202195+Animesh66@users.noreply.github.com>
2024-10-04 15:33:55 +0200

    Update leetcode_solutions.py

--- leetcode_solutions.py
+++ leetcode_solutions.py
@@ -893,4 +893,26 @@ def remove_all_occurance_of_k(input_list: list[int], k: int) -> list[int]:
     return [item for item in input_list if item != k], len([item for item in input_list if item != k])
 
 
-print(remove_all_occurance_of_k([0, 1, 2, 2, 3, 0, 4, 2], 2))
+# print(remove_all_occurance_of_k([0, 1, 2, 2, 3, 0, 4, 2], 2))
+
+"""
+Given two strings needle and haystack, 
+return the index of the first occurrence of needle in haystack, 
+or -1 if needle is not part of haystack.
+Example 1:
+Input: haystack = "sadbutsad", needle = "sad"
+Output: 0
+Explanation: "sad" occurs at index 0 and 6.
+The first occurrence is at index 0, so we return 0.
+Example 2:
+Input: haystack = "leetcode", needle = "leeto"
+Output: -1
+Explanation: "leeto" did not occur in "leetcode", so we return -1.
+"""
+
+
+def match_index_of_two_string(needle: str, haystack: str) -> int:
+    return haystack.find(needle)
+
+
+print(match_index_of_two_string("sad", "sadbutsad"))
