ba9b8e974534e7adf286811b361ca7b8170211b3
Animesh Mukherjee <37202195+Animesh66@users.noreply.github.com>
2024-10-08 20:23:44 +0200

    Update leetcode_solutions_easy.py

--- leetcode_solutions_easy.py
+++ leetcode_solutions_easy.py
@@ -25,10 +25,26 @@ Thus, the result should be [1,0].
 
 def increment_largest_int_by_one(input_list: list[int]) -> list[int]:
     input_int = 0
+    output_list = []
     for index, element in enumerate(input_list[::-1]):
+        if index == 0:
+            element += 1
         input_int += element * 10**index
-    input_int += 1
     return list(str(input_int))
 
 
 print(increment_largest_int_by_one([9]))
+
+"""
+Given two binary strings a and b, return their sum as a binary string.
+Example 1:
+Input: a = "11", b = "1"
+Output: "100"
+Example 2:
+Input: a = "1010", b = "1011"
+Output: "10101"
+Constraints:
+1 <= a.length, b.length <= 104
+a and b consist only of '0' or '1' characters.
+Each string does not contain leading zeros except for the zero itself.
+"""
